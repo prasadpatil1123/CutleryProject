8a94c263f1ca6c37bbc2abc12abefaa71232dd6c
prasadpatil1123 <122286888+prasadpatil1123@users.noreply.github.com>
2023-10-28 18:17:10 +0530

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+
+function App() {
+
+
+  return (
+    <>
+    <div>
+<Layout />
+
+  <h1 className='bg-green-300'>React router dom</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
